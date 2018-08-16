5aabba9d265dfab767349af256aa52085f068829
bambou8 <bambou8@localhost.localdomain>
2018-08-16 15:39:28 +0200

    scanner.py updated

--- TCPscanner/scanner.py
+++ TCPscanner/scanner.py
@@ -1,13 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding=utf-8 -*-
 import optparse
+from socket import *
+
 parser = optparse.OptionParser('usage %prog –H <target host> -p <target port>')
 parser.add_option('-H', dest='targetHost', type='string', help='specify target host')
 parser.add_option('-p', dest='targetPort', type='int', help='specify target port')
-(option, args) = parser.parse_args()
+(options, args) = parser.parse_args()
 if (options.targetHost == None) | (options.targetPort == None):
 	print parser.usage
 	exit(0)
 else:
 	tgtHost = options.targetHost
 	tgtPort = options.targetPort
+
+def connScan(tgtHost, tgtPort):
+	try:
+		connSock = socket(AF_INET, SOCK_STREAM)
+		connSock.connect((tgtHost, tgtPort))
+		print '[+]%d/tcp open'% tgtPort
+		connSock.close()
+	except Exception as e:
+		print '[-]%d/tcp closed'% tgtPort
