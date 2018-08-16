1afa8b4c030064a560e6cb6ff3fc1e8b429fe871
bambou8 <bambou8@localhost.localdomain>
2018-08-16 17:24:24 +0200

    scanner.py updated

--- TCPscanner/scanner.py
+++ TCPscanner/scanner.py
@@ -7,6 +7,7 @@ parser = optparse.OptionParser('usage %prog –H <target host> -p <target port>'
 parser.add_option('-H', dest='targetHost', type='string', help='specify target host')
 parser.add_option('-p', dest='targetPort', type='int', help='specify target port')
 (options, args) = parser.parse_args()
+
 if (options.targetHost == None) | (options.targetPort == None):
 	print parser.usage
 	exit(0)
@@ -14,7 +15,9 @@ else:
 	tgtHost = options.targetHost
 	tgtPort = options.targetPort
 
+
 def connScan(tgtHost, tgtPort):
+	print tgtHost+':'+tgtPort+'\n'
 	try:
 		connSock = socket(AF_INET, SOCK_STREAM)
 		connSock.connect((tgtHost, tgtPort))
@@ -22,3 +25,22 @@ def connScan(tgtHost, tgtPort):
 		connSock.close()
 	except Exception as e:
 		print '[-]%d/tcp closed'% tgtPort
+
+def portScan(tgtHost, tgtPorts):
+	print '<>portScan'
+	try:
+		tgtIP = gethostbyname(tgtHost)
+		print "[+]host resolved"
+	except:
+		print "[-] Cannot resolve '%s': Unknown host"%tgtHost
+		return
+	try:
+		tgtName = gethostbyaddr(tgtIP)
+		print '\n[+] Scan Results for: ' + tgtName[0]
+	except:
+		print '\n[+] Scan Results for: ' + tgtIP
+		setdefaulttimeout(1)
+	for tgtPort in tgtPorts:
+		print 'Scanning port ' + tgtPort
+		connScan(tgtHost, int(tgtPort))
+			
